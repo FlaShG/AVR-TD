11415254e56abbde10d3702c39415ca111fb443a
Sascha Graeff <sascha@blackicegames.de>
2013-07-26 18:10:04 +0200

    Added DisplayTouches and Touch Debug Mode for mouse.

--- Assets/Scripts/GUICircles.cs
+++ Assets/Scripts/GUICircles.cs
@@ -6,18 +6,23 @@ public class GUICircles
 	public static void Progress(Rect rect, float progress, Texture2D empty, Texture2D full)
     {
         float angle = progress * 360f;
+        Vector2 halfsize = new Vector2(rect.width, rect.height) / 2;
     
         GUI.BeginGroup(rect);
             GUI.DrawTexture(new Rect(0,0,rect.width,rect.height), empty);
             GUI.BeginGroup(new Rect(rect.width/2, 0, rect.width/2, rect.height));
                 
-                GUIUtility.RotateAroundPivot(angle, new Vector2(rect.width, rect.height));
-                GUI.BeginGroup(rect);
+                var matrix = GUI.matrix;
+                //GUI.BeginGroup(new Rect(0,0,rect.width,rect.height));
+                    GUIUtility.RotateAroundPivot(angle, new Vector2(0, halfsize.y));
+                    GUI.DrawTexture(new Rect(-halfsize.x, 0, rect.width, rect.height), full);
+                 //   GUIUtility.RotateAroundPivot(-angle, new Vector2(0, halfsize.y));
+                //GUI.EndGroup();
                 
-                GUI.DrawTexture(rect, full);
-                GUI.EndGroup();
+                GUI.matrix = matrix;
                 
             GUI.EndGroup();
+            GUI.matrix = matrix;
         GUI.EndGroup();
 	}
 }
