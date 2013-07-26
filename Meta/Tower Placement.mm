0c37fb79582bdbd75d1d6b0cc0d1746b837e8ed0
Sascha Graeff <sascha@blackicegames.de>
2013-07-26 14:35:44 +0200

    Added Met folder and Tower Placement Mind Map.

---
+++ Meta/Tower Placement.mm
@@ -0,0 +1,23 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1373554247251" ID="ID_475605285" MODIFIED="1373554268011" TEXT="Tower Placement">
+<node CREATED="1373554269361" ID="ID_592759176" MODIFIED="1373554491832" POSITION="right" TEXT="TowerPlacement">
+<node CREATED="1373554283429" ID="ID_1861989243" MODIFIED="1373555030004" TEXT="Neue Touches">
+<node CREATED="1373555030722" ID="ID_687139569" MODIFIED="1373555309764" TEXT="Im Freien =&gt; Neue Towerbaustelle, den Touch melden"/>
+<node CREATED="1373555045152" ID="ID_1792539180" MODIFIED="1373555216094" TEXT="Auf einer Baustelle =&gt; Der Baustelle den Touch melden"/>
+</node>
+</node>
+<node CREATED="1373554274349" ID="ID_1154962841" MODIFIED="1373554370492" POSITION="right" TEXT="Towerbaustelle">
+<node CREATED="1373554586610" ID="ID_1669469554" MODIFIED="1373554622592" TEXT="Story">
+<node CREATED="1373554624720" ID="ID_1508376261" MODIFIED="1373554792013" TEXT="Phase 1: 1s unsichtbar"/>
+<node CREATED="1373554636541" ID="ID_586519125" MODIFIED="1373555124574" TEXT="Phase 2: ns ladebalken1">
+<icon BUILTIN="help"/>
+</node>
+<node CREATED="1373554696781" ID="ID_54877260" MODIFIED="1373554746583" TEXT="tower platzieren, selbst zerst&#xf6;ren"/>
+</node>
+<node CREATED="1373554774831" ID="ID_262295148" MODIFIED="1373554805143" TEXT="Touch weg in Phase 1">
+<node CREATED="1373554808231" ID="ID_1122490294" MODIFIED="1373554808231" TEXT=""/>
+</node>
+</node>
+</node>
+</map>
